9a90eb8b8cf7e4d42760b7f89d299c099778821d
Ejaz Ansari <ejazahemed999@gmail.com>
2019-07-01 17:42:18 +0530

    Basic enhancements like debug log, .env added, Mysql connection pooling

--- server.js
+++ server.js
@@ -1,75 +1,95 @@
 
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const expressValidator = require('express-validator');
 const morgan = require('morgan');
-const rfs = require('rotating-file-stream');
 const path = require('path');
 const mongoose = require('mongoose');
-
-const config = require('./config');
+const fs = require('fs');
+const dotenv = require('dotenv');
+const chalk = require('chalk');
+const cors = require('cors');
 const app = express();
 const auth = require('./app/auth');
 const auth_controller = require('./app/auth/auth_controller');
 const product = require('./app/product'); // Imports routes for the products
+const log = require('Debug')('app:log');
 
-if(process.env.pro){
-	config.env = 'pro';
-}
+/**
+ * Load environment variables from .env file.
+ */
+dotenv.config({ path: process.env.pro ? '.env.pro' : '.env.dev' });
+log(process.env.pro ? chalk.red('Production Mode') : chalk.yellow('Development Mode'));
 
 //set secret
-app.set('Secret', config.secret);
-
-// Set up mongoose connection
-mongoose.connect(config.env === 'dev' ? config.mongo.dev.url : config.mongo.dev.url);
-mongoose.Promise = global.Promise;
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-// Set up Mysql connection
-global.connection = mysql.createConnection(config.env === 'dev' ? config.mysql.dev : config.mysql.pro);
-connection.connect();
-setInterval(function () {
-    global.connection.query('SELECT 1');
-}, 5000);
+app.set('Secret', process.env.SECRET);
 
 
-//Making /public folder static (/public will be the root of all files inside public)
-app.use(express.static(__dirname + '/public'));
+/**
+ * Set up mongoose connection
+ */
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('error', (err) => {
+	log(err);
+	log(chalk.red('MongoDB connection error. Please make sure MongoDB is running.'));
+	process.exit();
+});
 
-// // create a rotating write stream
-// var accessLogStream = rfs('access.log', {interval: '1d', path: path.join(__dirname, 'log/non_crash') });
-// var withCrashAccessLogStream = rfs('access.log', {interval: '1d', path: path.join(__dirname, 'log/with_crash') });
-// // setup the logger
-// app.use(morgan('combined', { stream: accessLogStream }));
-// app.use(morgan('combined', { stream: withCrashAccessLogStream, immediate: true }));
+
+/**
+ * Set up mysql connection pool
+ */
+global.connectionPool  = mysql.createPool({
+	connectionLimit : 10,
+	host            : process.env.MYSQL_HOST,
+	user            : process.env.MYSQL_USER,
+	password        : process.env.MYSQL_PASSWORD,
+	database        : process.env.MYSQL_DATABASE
+});
+
+global.connectionPool.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+	if (error){
+		log(error);
+		log(chalk.red('Mysql connection error. Please make sure Mysql is running.'));
+		process.exit();
+	}
+});
 
 
-//Validation middleware
-app.use(expressValidator());
+/**
+ * log only 4xx and 5xx responses to console
+ * log all requests to access.log
+ */
+app.use(morgan('dev', { skip: (req, res) => res.statusCode < 400 }));
+app.use(morgan('common', {stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' }) }));
 
-//Converting body into json
+
+// Converting body into json
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-//CORS related settings
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
-//Using routers from multiple files and folders
+// Enable Cross Origin Resource Sharing
+app.use(cors());
+
+// Making /public folder static (/public will be the root of all files inside public)
+app.use(express.static(__dirname + '/public'));
+
+// Validation middleware
+app.use(expressValidator());
+
+
+
+/**
+ * Using routers from multiple files and folders
+ */
 app.use('/auth', auth);
 app.use('/product', auth_controller.check, product);
 // app.use('/pdf', auth_controller.check, pdf);
 // app.use('/template', auth_controller.check, template);
 // app.use('/printer', auth_controller.check, printer);
 
-let port = 1234;
-app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
-    console.log('Production Mode ', process.env.pro);
+app.listen(process.env.PORT, () => {
+    log(chalk.bold('Server is up and running on ' + process.env.BASE_URL));
 });
